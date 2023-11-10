db3bb2f9563c318d9f29c15e7e14323f57d36882
chamois1 <nrabicenko5@gmail.com>
2023-11-11 01:27:36 +0200

    Added: unique page for user

--- src/main_app/views.py
+++ src/main_app/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def main_page(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        print(name)
+
+        return redirect("profile")
+
     return render(request, 'main_page.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
