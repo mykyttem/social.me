b054d819aab7adf4d54469d32327c879b61fbf00
chamois1 <nrabicenko5@gmail.com>
2023-11-11 15:24:03 +0200

    Addd data user on profile

--- src/main_app/views.py
+++ src/main_app/views.py
@@ -4,12 +4,23 @@ from django.shortcuts import render, redirect
 def main_page(request):
     if request.method == "POST":
         name = request.POST["name"]
-        print(name)
+        selected_social_network = request.POST.getlist("social_media")
 
+        # save session 
+        request.session["name"] = name
+        request.session["selected_social_network"] = selected_social_network
+
+        # redirect on profile user 
         return redirect("profile")
 
     return render(request, 'main_page.html')
 
 
 def profile(request):
-    return render(request, 'profile.html')
+    # get data from session
+    name = request.session.get("name")
+    selected_social_network = request.session.get("selected_social_network", [])
+
+    print(selected_social_network)
+
+    return render(request, 'profile.html', {'name': name, 'selected_social_network': selected_social_network})
